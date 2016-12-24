e9b8148b04e63954602e39d0a05997b195bbcac2
KarthicK Mariappan <karthick1.mariappan@gmail.com>
2016-12-24 06:36:49 -0500

    refactor: To add tab layout later.

--- MyPlaces/app/src/main/java/com/example/karthick/myplaces/MainActivityFragment.java
+++ MyPlaces/app/src/main/java/com/example/karthick/myplaces/MainActivityFragment.java
@@ -1,7 +1,9 @@
 package com.example.karthick.myplaces;
 
-import android.support.v4.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,28 @@ import android.view.ViewGroup;
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
+    }
+
+    /**
+     * Called to do initial creation of a fragment.  This is called after
+     * {@link #onAttach(Context)}  and before
+     * {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+     * <p>
+     * <p>Note that this can be called while the fragment's activity is
+     * still in the process of being created.  As such, you can not rely
+     * on things like the activity's content view hierarchy being initialized
+     * at this point.  If you want to do work once the activity itself is
+     * created, see {@link #onActivityCreated(Bundle)}.
+     * <p>
+     * <p>Any restored child fragments will be created before the base
+     * <code>Fragment.onCreate</code> method returns.</p>
+     *
+     * @param savedInstanceState If the fragment is being re-created from
+     *                           a previous saved state, this is the state.
+     */
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
