bab0c1b36c2dd9c5a247d638f8c1409a6f47ba58
KarthicK Mariappan <karthick1.mariappan@gmail.com>
2016-12-26 18:39:34 -0500

    feat: AdMob

--- GoPlaces/app/src/main/java/com/example/karthick/goplaces/ui/DetailActivity.java
+++ GoPlaces/app/src/main/java/com/example/karthick/goplaces/ui/DetailActivity.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 
 import com.example.karthick.goplaces.R;
 import com.example.karthick.goplaces.data.Place;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -62,6 +64,15 @@ public class DetailActivity extends AppCompatActivity {
                     startActivity(mapIntent);
                 }
             });
+
+            AdView mAdView = (AdView) findViewById(R.id.adView);
+            // Create an ad request. Check logcat output for the hashed device ID to
+            // get test ads on a physical device. e.g.
+            // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+            AdRequest adRequest = new AdRequest.Builder()
+                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                    .build();
+            mAdView.loadAd(adRequest);
 
         }
     }
